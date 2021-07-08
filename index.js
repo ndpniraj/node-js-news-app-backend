2549d3f3282370cc22cfb39aaf3e888b2e45487f
ndpniraj <ndpniraj@gmail.com>
2021-07-08 13:23:43 +0530

    added morgan

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const newsRouter = require('./routers/news');
 
+// added later to find out incoming and outgoing requests. 
+const morgan = require('morgan')
+app.use(morgan('dev'))
+
 app.use(express.static('public'));
 app.use(express.static('data/uploads'));
 app.use('/api', newsRouter);
